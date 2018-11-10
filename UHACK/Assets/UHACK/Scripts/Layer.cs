30db03c14686d4cdafa37db1493cc71b5d433cb0
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2018-11-09 22:39:16 -0600

    Fixed - Refactored Unity Repo.

---
+++ UHACK/Assets/UHACK/Scripts/Layer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LayerType{
+    Conv,
+    Pool,
+    Full,
+    Dense,
+    Flatten,
+    Dropout
+}
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(OVRGrabbable))]
+public class Layer : MonoBehaviour {
+    //public bool isBeingHeld = true;
+    
+    public new Rigidbody rigidbody;
+    public OVRGrabbable grabbable;
+
+    public LayerType layerType;
+
+    private LayerManger manager;
+
+    public Color color;
+
+    public bool isBeingHeld()
+    {
+        return true;
+        //return grabbable.isGrabbed;
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UHACK/Assets/UHACK/Scripts/LayerManger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManger : MonoBehaviour {
+
+    const int NUM_LAYERS = 4;
+
+    SnapContainer[] Containers;
+
+    private void Awake()
+    {
+        SnapContainer[] tempContainers =  FindObjectsOfType<SnapContainer>();
+
+        if (NUM_LAYERS != tempContainers.Length) {
+            Debug.LogError("ERROR: Container ID's not set. Check that NUM_LAYERS is of correct length.");
+            return;
+        }
+
+        Containers = new SnapContainer[NUM_LAYERS];
+
+        // Order Containers to be in the order of SnapContainers.
+        for (int i = 0; i < NUM_LAYERS; i++)
+        {
+            int ID = tempContainers[i].ContainerID;
+            Containers[ID] = tempContainers[i];
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
