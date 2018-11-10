cdf353dae79a4cc0c39ddc03868762e636f14ae1
20zinnm <meyerzinn@gmail.com>
2018-11-10 00:19:52 -0600

    fix coroutine maybe?

--- UHACK/Assets/UHACK/Scripts/Testing evaluator/TestingEvaluator.cs
+++ UHACK/Assets/UHACK/Scripts/Testing evaluator/TestingEvaluator.cs
@@ -9,36 +9,42 @@ using System.Threading;
 
 public class TestingEvaluator : MonoBehaviour
 {
-    [SerializeField]
-    Button button;
-
     // Use this for initialization
     void Start()
     {
         Channel channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+        Debug.Log("Created channel.");
         try
         {
             var client = new Evaluator.Evaluator.EvaluatorClient(channel);
+            Debug.Log("Created client.");
             ConvolutionLayer conv = new ConvolutionLayer { Filters = 1 };
+            Debug.Log("Created concolution layer.");
             FlattenLayer flatten = new FlattenLayer { };
+            Debug.Log("Created flatten layer.");
             EvaluateRequest req = new EvaluateRequest { Layers = { new Evaluator.Layer { Convolution = conv }, new Evaluator.Layer { Flatten = flatten } } };
+            Debug.Log("Created evaluate request.");
             using (var call = client.Evaluate(req))
             {
-                var responseReaderTask = Task.Run(async () =>
-                {
-                    while (await call.ResponseStream.MoveNext(CancellationToken.None)) {
-                        var progress = call.ResponseStream.Current;
-                        Debug.Log(progress.Accuracy);
-                    }
-                });
+                Debug.Log("Invoked evaluate.");
+                StartCoroutine("DoRequest", call);
             }
-            //channel.ShutdownAsync().Wait();
+            channel.ShutdownAsync().Wait();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Debug.LogError(e);
             throw;
         }
+    }
+
+    IEnumerable DoRequest(AsyncServerStreamingCall<ProgressUpdate> call) {
+        while (call.ResponseStream.MoveNext(CancellationToken.None).Result)
+        {
+            var progress = call.ResponseStream.Current;
+            Debug.Log(progress.Accuracy);
+        }
+        yield return null;
     }
 
     // Update is called once per frame
